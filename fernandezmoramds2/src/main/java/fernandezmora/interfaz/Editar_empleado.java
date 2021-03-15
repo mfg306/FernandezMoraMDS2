148f27bf436a1f30c798d2964d6b67aac3eb3828
Marta Fernández <mfg306@inlumine.ual.es>
2021-03-15 22:02:34 +0100

    Incorporando el cancelar edicion de empleado

--- fernandezmoramds2/src/main/java/fernandezmora/interfaz/Editar_empleado.java
+++ fernandezmoramds2/src/main/java/fernandezmora/interfaz/Editar_empleado.java
@@ -1,10 +1,26 @@
 package fernandezmora.interfaz;
 
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
 import vistas.VistaEditar_empleado;
 
 public class Editar_empleado extends VistaEditar_empleado {
 
 	public Empleado _empleado;
+	public VerticalLayout layout;
 	
+	public Editar_empleado() {
+		layout = this.getVaadinVerticalLayout().as(VerticalLayout.class);
+		cancelar_edicion();
+	}
+
 	
+	public void cancelar_edicion() {
+		this.getVaadinButton1().addClickListener(event ->{
+			Gestionar_empleados ge = new Gestionar_empleados();
+			this.getVaadinVerticalLayout1().setVisible(false);
+			this.getH1().setVisible(false);
+			layout.add(ge);
+		});
+	}
 }
