7fc48ae0fa73582a2edc13a83ce88d2981b19710
Marta Fernández <mfg306@inlumine.ual.es>
2021-03-15 22:43:58 +0100

    Creacion de producto

--- fernandezmoramds2/src/main/java/fernandezmora/interfaz/Crear_producto.java
+++ fernandezmoramds2/src/main/java/fernandezmora/interfaz/Crear_producto.java
@@ -2,17 +2,29 @@ package fernandezmora.interfaz;
 
 import java.util.Vector;
 
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
 import vistas.VistaCrear_producto;
 
 public class Crear_producto extends VistaCrear_producto {
-	/*private TextField _nombre_productoTF;
-	private TextField _precioTF;
-	private TextField _caracteristicasTF;
-	private Button _guardarB;
-	private Button _cancelarB;
-	private Button _anadir_imagenB;
-	private Panel _panel_producto;
-	private Panel _panel_caracteristicas;*/
 	public Producto_administrador _producto_administrador;
 	public Vector<Imagen> _imagen = new Vector<Imagen>();
+	public VerticalLayout layout;
+
+	
+	public Crear_producto() {
+		layout = this.getVaadinVerticalLayout().as(VerticalLayout.class);
+		cancelar_creacion();
+	}
+	
+	
+	public void cancelar_creacion() {
+		this.getVaadinButton2().addClickListener(event ->{
+			Gestionar_productos gp = new Gestionar_productos();
+			this.getH1().setVisible(false);
+			this.getVaadinHorizontalLayout().setVisible(false);
+			this.getVaadinHorizontalLayout1().setVisible(false);
+			layout.add(gp);
+		});
+	}
 }
--- fernandezmoramds2/src/main/java/fernandezmora/interfaz/Gestionar_productos.java
+++ fernandezmoramds2/src/main/java/fernandezmora/interfaz/Gestionar_productos.java
@@ -10,33 +10,43 @@ public class Gestionar_productos extends VistaGestionar_productos {
 	public Editar_producto _editar_producto;
 	public VerticalLayout layout;
 
-	
 	public Gestionar_productos() {
 		this._productos_administrador = new Productos_administrador();
 		this._productos_administrador.add_Productos();
 		this._productos_administrador.add_Productos();
-		
-		layout = this.getVaadinVerticalLayout().as(VerticalLayout.class);	
-		
+
+		layout = this.getVaadinVerticalLayout().as(VerticalLayout.class);
+
 		layout.add(this._productos_administrador);
-		
+
 		editar_Producto();
+		crear_Producto();
 	}
-	
+
 	public void ocultar_Gestionar_Ofertas() {
 		layout.remove(this._productos_administrador);
 		this.getVaadinButton().setVisible(false);
 		this.getH1().setVisible(false);
 	}
-	
+
 	public void editar_Producto() {
-		for(Producto_administrador pa : this._productos_administrador._list_Producto_administrador) {
-			pa.getVaadinButton().addClickListener(event ->{
+		for (Producto_administrador pa : this._productos_administrador._list_Producto_administrador) {
+			pa.getVaadinButton().addClickListener(event -> {
 				pa._editar_producto = new Editar_producto();
 				this.ocultar_Gestionar_Ofertas();
 				layout.add(pa._editar_producto);
 			});
 		}
 	}
-	
+
+	public void crear_Producto() {
+		this.getVaadinButton().addClickListener(event ->{
+			ocultar_Gestionar_Ofertas();
+			
+			Crear_producto cp = new Crear_producto();
+			layout.add(cp);
+		});
+
+	}
+
 }
