6ff9713c825d3d277e048f45589aab0e3a600012
Marta Fernández <mfg306@inlumine.ual.es>
2021-03-18 22:26:46 +0100

    jjejeje x222

--- fernandezmoramds2/src/main/java/vistas/VistaVercategorias.java
+++ fernandezmoramds2/src/main/java/vistas/VistaVercategorias.java
@@ -4,6 +4,8 @@ import com.vaadin.flow.templatemodel.TemplateModel;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.component.polymertemplate.Id;
 
 /**
  * A Designer generated component for the vista-vercategorias template.
@@ -15,7 +17,10 @@ import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 @JsModule("./src/vista-vercategorias.js")
 public class VistaVercategorias extends PolymerTemplate<VistaVercategorias.VistaVercategoriasModel> {
 
-    /**
+    @Id("vaadinVerticalLayout")
+	private Element vaadinVerticalLayout;
+
+	/**
      * Creates a new VistaVercategorias.
      */
     public VistaVercategorias() {
@@ -28,4 +33,12 @@ public class VistaVercategorias extends PolymerTemplate<VistaVercategorias.Vista
     public interface VistaVercategoriasModel extends TemplateModel {
         // Add setters and getters for template properties here.
     }
+
+	public Element getVaadinVerticalLayout() {
+		return vaadinVerticalLayout;
+	}
+
+	public void setVaadinVerticalLayout(Element vaadinVerticalLayout) {
+		this.vaadinVerticalLayout = vaadinVerticalLayout;
+	}
 }
