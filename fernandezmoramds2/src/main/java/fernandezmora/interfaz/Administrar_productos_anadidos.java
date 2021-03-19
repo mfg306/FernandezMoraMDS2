74383f0f58122d5ad7472faac56244662f0bc912
Marta Fernández <mfg306@inlumine.ual.es>
2021-03-19 13:48:10 +0100

    Subiendo cambios

--- fernandezmoramds2/src/main/java/fernandezmora/interfaz/Administrar_productos_anadidos.java
+++ fernandezmoramds2/src/main/java/fernandezmora/interfaz/Administrar_productos_anadidos.java
@@ -6,17 +6,17 @@ import vistas.VistaAdministrar_productos_anadidos;
 
 public class Administrar_productos_anadidos extends VistaAdministrar_productos_anadidos{
 	public Productos_listado_administracion _productos_listado_administracion;
-	public VerticalLayout layout;
+	public VerticalLayout layoutIzquierda;
 	
 	public  Administrar_productos_anadidos() {
 		this._productos_listado_administracion = new Productos_listado_administracion();
 		
-		this.layout = this.getBotonesCG().as(VerticalLayout.class);
+		this.layoutIzquierda = this.getHuecoIzquierda().as(VerticalLayout.class);
 		this._productos_listado_administracion.add_productos_listado_administracion();
 		this._productos_listado_administracion.add_productos_listado_administracion();
 		this._productos_listado_administracion.add_productos_listado_administracion();
 		
-		layout.add(this._productos_listado_administracion);
+		layoutIzquierda.add(this._productos_listado_administracion);
 	}
 	
 	
