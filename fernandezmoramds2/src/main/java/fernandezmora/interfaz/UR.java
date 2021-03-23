40568227d8f9be2cc7d4f68f9c2284be7d4bb202
JORGE MORA CAMPOY <jmc617@inlumine.ual.es>
2021-03-24 00:11:11 +0100

    Comienzo UR

--- fernandezmoramds2/src/main/java/fernandezmora/interfaz/UR.java
+++ fernandezmoramds2/src/main/java/fernandezmora/interfaz/UR.java
@@ -8,13 +8,11 @@ public class UR extends UR_UNR {
 	//public iUR _iUR;
 	public Menu_UR _menu_UR;
 	public Ver_producto_UR _ver_producto_UR;
-	public VerticalLayout layout;
+	
 	
 	public UR() {
 		_menu_UR = new Menu_UR();
-		layout = this.getVaadinVerticalLayout().as(VerticalLayout.class);
-		layout.add(_menu_UR);
-		
-			
+		this.getHuecoMenu().as(VerticalLayout.class).add(_menu_UR);
+				
 	}
 }
--- fernandezmoramds2/src/main/java/fernandezmora/interfaz/Ver_producto_UR.java
+++ fernandezmoramds2/src/main/java/fernandezmora/interfaz/Ver_producto_UR.java
@@ -2,8 +2,6 @@ package fernandezmora.interfaz;
 
 public class Ver_producto_UR extends Producto_UNR {
 	
-
-
 	public UR _uR;
 	
 	public void Comentar() {
