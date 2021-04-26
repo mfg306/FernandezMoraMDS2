7dfcbe2dfe1905160100a78dbece177e3864bdd1
Marta Fernández <mfg306@inlumine.ual.es>
2021-04-26 23:12:44 +0200

    Actualizando el pom.xml

--- fernandezmoramds2/src/main/java/fernandezmora/interfaz/Registrarse.java
+++ fernandezmoramds2/src/main/java/fernandezmora/interfaz/Registrarse.java
@@ -19,12 +19,12 @@ public class Registrarse extends VistaRegistrarse implements Serializable {
 	public Registrarse(Iniciar_sesion_UNR iunr) {
 		inicializar(iunr);
 	}
-	
+
 	public void inicializar(Iniciar_sesion_UNR iunr) {
 		this._iniciar_sesion_UNR = iunr;
 		abrir_Iniciar_Sesion_Registrarse();
-		
-		this.getBoton_registrarse().addClickListener(event ->{
+
+		this.getBoton_registrarse().addClickListener(event -> {
 			try {
 				registrarse();
 			} catch (PersistentException e) {
@@ -40,22 +40,12 @@ public class Registrarse extends VistaRegistrarse implements Serializable {
 			this._iniciar_sesion_UNR._menu_UNR.layout.add(this._iniciar_sesion_UNR);
 		});
 	}
-	
-	public void registrarse() throws PersistentException {
-		//iUNR_ iunr = new BDPrincipal();
-		PersistentTransaction t = base_de_datos.HitoPersistentManager.instance().getSession().beginTransaction();
 
-		try {
-			
-			base_de_datos.Usuario_General base_de_DatosUR = base_de_datos.Usuario_GeneralDAO.createUsuario_General();
-			// TODO Initialize the properties of the persistent object here, the following properties must be initialized before saving : _Valora, _Envia, _recibido, _Compra, _Es_de_un, esta_operativo
-			base_de_datos.Usuario_GeneralDAO.save(base_de_DatosUR);
-			t.commit();
-		}
-		catch (Exception e) {
-			t.rollback();
-		}
-	//iunr.registrarse(this.getNombre().getValue(), this.getApellidos().getValue(), this.getApellidos().getValue(), this.getContrasenia().getValue(), this.getConfirmacionContrasenia().getValue(), iunr);
+	public void registrarse() throws PersistentException {
+		iUNR_ iunr = new BDPrincipal();
+
+		iunr.registrarse(this.getNombre().getValue(), this.getApellidos().getValue(), this.getApellidos().getValue(),
+				this.getContrasenia().getValue(), this.getConfirmacionContrasenia().getValue(), iunr);
 	}
 
 }
