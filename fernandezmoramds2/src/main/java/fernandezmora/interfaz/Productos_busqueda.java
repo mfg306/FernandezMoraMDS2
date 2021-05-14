7eee19bee7fb3e757028d6912abf6fad23327d77
Jorge617 <73338384+Jorge617@users.noreply.github.com>
2021-05-15 01:36:29 +0200

    Update Productos_busqueda.java

--- fernandezmoramds2/src/main/java/fernandezmora/interfaz/Productos_busqueda.java
+++ fernandezmoramds2/src/main/java/fernandezmora/interfaz/Productos_busqueda.java
@@ -2,11 +2,21 @@ package fernandezmora.interfaz;
 
 import java.util.Vector;
 
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
 import vistas.VistaProductos_busqueda;
 
 public class Productos_busqueda extends VistaProductos_busqueda{
 	public Buscador _buscador;
-	public Vector<Producto_busqueda> _list_Producto_busqueda = new Vector<Producto_busqueda>();
+	public Vector<Producto_busqueda> _list_Producto_busqueda;
+	public VerticalLayout layout;
+	
+	public Productos_busqueda(Buscador buscador) {
+		this._buscador = buscador;
+		this._list_Producto_busqueda = new Vector<Producto_busqueda>();
+		this.layout = this.getVaadinVerticalLayout().as(VerticalLayout.class);
+	
+	}
 
 	public void Ver_anteriores() {
 		throw new UnsupportedOperationException();
