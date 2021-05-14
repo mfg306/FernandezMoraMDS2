d13dd2c3734bdc988b822452fda2c6ebe98ae339
Jorge617 <73338384+Jorge617@users.noreply.github.com>
2021-05-15 01:35:57 +0200

    Update Producto_busqueda.java

--- fernandezmoramds2/src/main/java/fernandezmora/interfaz/Producto_busqueda.java
+++ fernandezmoramds2/src/main/java/fernandezmora/interfaz/Producto_busqueda.java
@@ -3,6 +3,13 @@ package fernandezmora.interfaz;
 import vistas.VistaProducto_busqueda;
 
 public class Producto_busqueda extends VistaProducto_busqueda{
-	private Producto _producto;
+	public Producto _producto;
 	public Productos_busqueda _productos_busqueda;
+	
+	public Producto_busqueda(Productos_busqueda productos_busqueda,base_de_datos.Producto p) {
+		this.getNombre_producto().setText(p.getNombre());
+		this.getPrecio_producto().setText(String.valueOf(p.getPrecio_producto()));
+		this._productos_busqueda = productos_busqueda;
+		
+	}
 }
