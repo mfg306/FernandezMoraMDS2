6601b4a31831dcd1b188bdfd7b04c2e92a42ea85
Marta Fernández <mfg306@inlumine.ual.es>
2021-05-19 16:14:50 +0200

    Arreglando oferta

--- fernandezmoramds2/src/main/java/interfaz/Oferta.java
+++ fernandezmoramds2/src/main/java/interfaz/Oferta.java
@@ -4,35 +4,32 @@ import com.vaadin.flow.component.notification.Notification;
 
 import vistas.VistaOferta;
 
-public class Oferta extends VistaOferta{
-	
+public class Oferta extends VistaOferta {
+
 	public Ofertas _ofertas;
 	public Productos_oferta _productos_oferta;
 	public base_de_datos.Oferta oferta;
-	
-	public Oferta(Ofertas listaOfertas, UR_UNR unrunr,base_de_datos.Oferta of) {
-		inicializar(listaOfertas, unrunr,of);
+
+	public Oferta(Ofertas listaOfertas, UR_UNR unrunr, base_de_datos.Oferta of) {
+		inicializar(listaOfertas, unrunr, of);
 	}
 
-	public void inicializar(Ofertas listaOfertas, UR_UNR unrunr,base_de_datos.Oferta of) {
+	public void inicializar(Ofertas listaOfertas, UR_UNR unrunr, base_de_datos.Oferta of) {
 		this._ofertas = listaOfertas;
 		this._productos_oferta = new Productos_oferta(this, this._ofertas._uR_UNR);
 		this.oferta = of;
 		this.getBotonOferta().setText(of.getNombre_Oferta());
 		abrir_Oferta();
 	}
-	
+
 	public void abrir_Oferta() {
-		for (Oferta of : this._ofertas._list_Ofertas) {
-			of.getBotonOferta().addClickListener(event -> {
-				this._ofertas._uR_UNR.layoutOfertas.remove(this._ofertas);
-				this._ofertas._uR_UNR.getBotonVerCategorias().setVisible(false);
-				this._ofertas._uR_UNR.layoutProductosMasVendidosPorCategorias.setVisible(false);
-				this._ofertas._uR_UNR.layoutOfertas.add(of._productos_oferta);
-				Notification.show("AQUI");
-			});
-		}
+		this.getBotonOferta().addClickListener(event -> {
+			System.out.println("Click en oferta");
+			this._ofertas._uR_UNR.layoutOfertas.remove(this._ofertas);
+			this._ofertas._uR_UNR.getBotonVerCategorias().setVisible(false);
+			this._ofertas._uR_UNR.layoutProductosMasVendidosPorCategorias.setVisible(false);
+			this._ofertas._uR_UNR.layoutOfertas.add(this._productos_oferta);
+		});
 	}
-	
 
 }
