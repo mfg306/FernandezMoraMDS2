7c299014023744de5747e43a2c72676e743f7d3c
Marta Fernández <mfg306@inlumine.ual.es>
2021-05-22 12:30:35 +0200

    Arreglando fechas

--- fernandezmoramds2/src/main/java/base_de_datos/BD_Mensajes.java
+++ fernandezmoramds2/src/main/java/base_de_datos/BD_Mensajes.java
@@ -110,7 +110,7 @@ public class BD_Mensajes {
 			t.rollback();
 		}
 		
-		HitoPersistentManager.instance().disposePersistentManager();
+//		HitoPersistentManager.instance().disposePersistentManager();
 
 		return false;
 	}
--- fernandezmoramds2/src/main/java/interfaz/Editar_categoria.java
+++ fernandezmoramds2/src/main/java/interfaz/Editar_categoria.java
@@ -38,7 +38,7 @@ public class Editar_categoria extends Zona_productos {
 		this.getCampoOferta().setVisible(false);
 		this.getCampoOferta1().setVisible(false);
 
-		this.getCampoFechaCaducidad().setVisible(false);
+		this.getCampoFecha().setVisible(false);
 		this.getFechaCaducidad().setVisible(false);
 
 		this.getCampoCategoria().setValue(ca.categoria.getNombre_categoria());
@@ -55,7 +55,7 @@ public class Editar_categoria extends Zona_productos {
 		this.getH21().setVisible(false);
 		this.getVaadinHorizontalLayout().setVisible(false);
 
-		this.getCampoFechaCaducidad().setVisible(false);
+		this.getCampoFecha().setVisible(false);
 
 		this.getCampoCategoria().setVisible(false);
 		this.getNombreCategoria().setVisible(false);
@@ -81,9 +81,9 @@ public class Editar_categoria extends Zona_productos {
 		this.getVaadinButton().addClickListener(event -> {
 			iAdministrador admin = new BDPrincipal();
 
-			SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 			Date date = new Date();
-			formatter.format(date);
+			String fechaActual = formatter.format(date);
 
 			Vector<Producto_listado_administracion> listaProductos = this._productos_listado_administracion._list_Producto_listado_administracion;
 			base_de_datos.Producto[] productosCategoria = new base_de_datos.Producto[listaProductos.size()];
@@ -99,8 +99,8 @@ public class Editar_categoria extends Zona_productos {
 				}
 				
 				
-				this._categoria_administrador.categoria = admin.actualizarCategoria(this.getCampoCategoria().getValue(), productosCategoria, date.toString(),
-						this._categoria_administrador.categoria);
+				this._categoria_administrador.categoria = admin.actualizarCategoria(this.getCampoCategoria().getValue(), 
+						productosCategoria, fechaActual, this._categoria_administrador.categoria);
 				Notification.show("Categoria creada con éxito");
 				retroceder();
 			} catch (PersistentException e) {
