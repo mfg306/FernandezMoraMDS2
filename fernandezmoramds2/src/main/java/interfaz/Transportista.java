89d1c29696d8171e5d083fffbb6ad598fe6e28ef
Marta Fernández <mfg306@inlumine.ual.es>
2021-05-23 15:11:29 +0200

    Pedidos Transportista se actualizan al marcarlos

--- fernandezmoramds2/src/main/java/interfaz/Transportista.java
+++ fernandezmoramds2/src/main/java/interfaz/Transportista.java
@@ -3,6 +3,7 @@ package interfaz;
 import org.orm.PersistentException;
 
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 import basededatos.BDPrincipal;
@@ -18,16 +19,28 @@ public class Transportista extends VistaTransportista {
 	
 	public Transportista(base_de_datos.Transportista transportista) {
 		this.transportista = transportista;
+		layout = this.getHuecoListaPedidos().as(VerticalLayout.class);
+
+
 		cerrar_sesion();
 		inicializar();
 	}
 	
 	public void inicializar() {
+		if(this._pedidos_T != null) {
+			layout.remove(this._pedidos_T);
+		}
+		
 		this._pedidos_T = new Pedidos_T(this);
-				
-		layout = this.getHuecoListaPedidos().as(VerticalLayout.class);
-		layout.add(this._pedidos_T);
+			
 		cargarPedidosT();
+		
+		if(this._pedidos_T._list_Pedido_T.size() > 0) {
+			layout.add(this._pedidos_T);
+
+		} else {
+			Notification.show("No tiene ningun pedido para repartir.");
+		}
 	}
 	
 	public void ocultar_Transportista() {
--- fernandezmoramds2/src/main/java/interfaz/Venta.java
+++ fernandezmoramds2/src/main/java/interfaz/Venta.java
@@ -8,37 +8,37 @@ public class Venta extends VistaVenta{
 	
 	public Venta(base_de_datos.Compra c) {
 		this.compra = c;
-		this.getLabel().setText(this.compra.getPrecio_total() + "\n" + this.compra.getFecha_estado());
+		this.getLabel().setText(this.compra.getPrecio_total() + " € " + System.lineSeparator() + this.compra.getFecha_estado());
 		
 		String descripcion = "";
 		
 		if(compra instanceof base_de_datos.Pendiente) {
-			descripcion += "Estado: Pendiente";
+			descripcion += "Estado: Pendiente\n" + System.lineSeparator();
 			
 			compra = (base_de_datos.Pendiente)compra;
 			
-			descripcion += "Cliente: " + ((base_de_datos.Pendiente) compra).get_Hace_compra().getCorreo_electronico() + " \n";
-			descripcion += "Encargado: " + ((base_de_datos.Pendiente) compra).get_Encargado_de_compras().getCorreo();
+			descripcion += "Cliente: " + ((base_de_datos.Pendiente) compra).get_Hace_compra().getCorreo_electronico() + System.lineSeparator();
+			descripcion += "Encargado: " + ((base_de_datos.Pendiente) compra).get_Encargado_de_compras().getCorreo() + System.lineSeparator();
 			
 		}
 		
 		if(compra instanceof base_de_datos.Enviado) {
-			descripcion += "Estado: Enviado";
+			descripcion += "Estado: Enviado\n" +  System.lineSeparator();
 			
 			compra = (base_de_datos.Enviado)compra;
 			
-			descripcion += "Pedido repartido al transportista : " +  ((base_de_datos.Enviado)compra).get_Transportista().getCorreo();
+			descripcion += "Pedido repartido al transportista : " +  ((base_de_datos.Enviado)compra).get_Transportista().getCorreo() + System.lineSeparator();
 			
 			
 		}
 		
 		if(compra instanceof base_de_datos.Recibido) {
-			descripcion += "Estado: Recibido";
+			descripcion += "Estado: Recibido\n" +  System.lineSeparator();
 			
 			compra = (base_de_datos.Recibido)compra;
 			
 			if(((base_de_datos.Recibido) compra).getRecibido()) {
-				descripcion += "Pedido recibido por : " + ((base_de_datos.Recibido) compra).get_recibe().getCorreo_electronico();
+				descripcion += "Pedido recibido por : " + ((base_de_datos.Recibido) compra).get_recibe().getCorreo_electronico()  + System.lineSeparator();
 			}
 		}
 				
