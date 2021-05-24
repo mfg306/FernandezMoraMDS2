2d43599a7e5425b7304619f0ca8a0b7a14a07c9d
JORGE MORA CAMPOY <jmc617@inlumine.ual.es>
2021-05-24 18:52:28 +0200

    Subiendo mostrar productos mas vendidos

--- fernandezmoramds2/src/main/java/interfaz/Productos_mas_vendidos_por_categorias.java
+++ fernandezmoramds2/src/main/java/interfaz/Productos_mas_vendidos_por_categorias.java
@@ -18,6 +18,7 @@ public class Productos_mas_vendidos_por_categorias extends VistaProductos_mas_ve
 	public Vector<Producto_mas_vendido_por_categoria> _list_Producto_mas_vendido_por_categoria = new Vector<Producto_mas_vendido_por_categoria>();
 	public Select<String> categoriasMasVendidos = new Select<>();
 	public VerticalLayout layout;
+	boolean inicial = true;
 
 	public Productos_mas_vendidos_por_categorias(UR_UNR urunr) {
 		layout = this.getListaCategoriasMasVendidos().as(VerticalLayout.class);
@@ -30,13 +31,17 @@ public class Productos_mas_vendidos_por_categorias extends VistaProductos_mas_ve
 		this._list_Producto_mas_vendido_por_categoria = new Vector<Producto_mas_vendido_por_categoria>();
 		
 		layout.add(this.categoriasMasVendidos);
-		
+		if(inicial) {
+		 Clasificar_por_categoria_inicial();
+		}else {
+			Clasificar_por_categoria();
+		}
 		Clasificar_por_categoria();
 		abrir_Producto_MasVendido_Por_Categoria();
 	}
 
 	public void add_Producto_Mas_Vendido_Por_Categoria(base_de_datos.Producto p) {
-		Producto_mas_vendido_por_categoria pmc = new Producto_mas_vendido_por_categoria(this, this._uR_UNR,p);
+		Producto_mas_vendido_por_categoria pmc = new Producto_mas_vendido_por_categoria(this, this._uR_UNR, p);
 		this._list_Producto_mas_vendido_por_categoria.add(pmc);
 		this.getLista_productos_categoria().add(pmc);
 	}
@@ -52,47 +57,62 @@ public class Productos_mas_vendidos_por_categorias extends VistaProductos_mas_ve
 			});
 		}
 	}
-	
+
 	public void cargarCategorias() {
 		iUR_UNR iurunr = new BDPrincipal();
-		
-		base_de_datos.Categoria[] categorias =  iurunr.cargarCategorias();
+
+		base_de_datos.Categoria[] categorias = iurunr.cargarCategorias();
 		ArrayList<String> listaCategorias = new ArrayList<>();
-		
-		for(base_de_datos.Categoria c : categorias) {
+
+		for (base_de_datos.Categoria c : categorias) {
 			listaCategorias.add(c.getNombre_categoria());
 		}
-		
-		
-		if(listaCategorias.size() != 0) {
+
+		if (listaCategorias.size() != 0) {
 			this.categoriasMasVendidos.setItems(listaCategorias);
 			this.categoriasMasVendidos.setValue(listaCategorias.get(0));
 		}
 	}
-	
+
+	public void Clasificar_por_categoria_inicial() {
+		iUR_UNR iur = new BDPrincipal();
+		try {
+			base_de_datos.Categoria[] categorias = iur.cargarCategorias();
+			base_de_datos.Producto[] productos = iur.cargarProductosMasVendidos(categorias[0].getNombre_categoria());
+
+			if (productos != null) {
+				for (base_de_datos.Producto p : productos) {
+					this.add_Producto_Mas_Vendido_Por_Categoria(p);
+				}
+			}
+
+			//this.inicializar(_uR_UNR);
+		} catch (PersistentException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public void Clasificar_por_categoria() {
-		this.categoriasMasVendidos.addValueChangeListener(event ->{
+		this.inicial = false;
+		this.categoriasMasVendidos.addValueChangeListener(event -> {
 			this.getLista_productos_categoria().removeAll();
-
 			iUR_UNR iurunr = new BDPrincipal();
-			
+
 			try {
-				base_de_datos.Producto[] productos = iurunr.cargarProductosMasVendidos(this.categoriasMasVendidos.getValue());
-				
-				if(productos != null) {
-					for(base_de_datos.Producto p : productos) {
+				base_de_datos.Producto[] productos = iurunr
+						.cargarProductosMasVendidos(this.categoriasMasVendidos.getValue());
+
+				if (productos != null) {
+					for (base_de_datos.Producto p : productos) {
 						this.add_Producto_Mas_Vendido_Por_Categoria(p);
-					}	
+					}
 				}
-				
+
 				this.inicializar(_uR_UNR);
 			} catch (PersistentException e) {
 				e.printStackTrace();
 			}
 		});
-		
 
-		
 	}
 }
