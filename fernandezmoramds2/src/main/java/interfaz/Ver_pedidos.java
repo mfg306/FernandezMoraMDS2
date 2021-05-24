477b5da2ad1174bb83619ae2c8564380529841d4
Marta Fernández <52571378+mfg306@users.noreply.github.com>
2021-05-24 17:29:51 +0200

    Update Ver_pedidos.java

--- fernandezmoramds2/src/main/java/interfaz/Ver_pedidos.java
+++ fernandezmoramds2/src/main/java/interfaz/Ver_pedidos.java
@@ -2,6 +2,8 @@ package interfaz;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
+import basededatos.BDPrincipal;
+import basededatos.iUR;
 import vistas.VistaVer_pedidos;
 
 public class Ver_pedidos extends VistaVer_pedidos {
@@ -18,6 +20,29 @@ public class Ver_pedidos extends VistaVer_pedidos {
 		this._menu_UR = menuur;
 		this._pedidos = new Pedidos(this);
 		this.layout.add(this._pedidos);
+		
+		verPedidos();
+	}
+	
+	public void verPedidos() {
+		iUR iur = new BDPrincipal();
+		
+		base_de_datos.Pendiente[] listaPendientes = iur.cargarPendientes(this._menu_UR._uR.UR);
+		
+		if(listaPendientes != null) {
+			for(base_de_datos.Pendiente p : listaPendientes) {
+				this._pedidos.add_pedidos(p);
+			}
+		}
+
+		base_de_datos.Recibido[] listaRecibidos = iur.cargarRecibidos(this._menu_UR._uR.UR);
+		
+		if(listaRecibidos != null) {
+			for(base_de_datos.Recibido r : listaRecibidos) {
+				this._pedidos.add_pedidos(r);
+			}
+		}
+
 		
 	}
 	
