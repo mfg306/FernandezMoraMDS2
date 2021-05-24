e2f1a34ed5e19fe2379e89ec0ed6ea6fb7c92594
Marta Fernández <52571378+mfg306@users.noreply.github.com>
2021-05-24 17:30:10 +0200

    Update Pedido.java

--- fernandezmoramds2/src/main/java/interfaz/Pedido.java
+++ fernandezmoramds2/src/main/java/interfaz/Pedido.java
@@ -8,18 +8,29 @@ import vistas.VistaPedido;
 public class Pedido extends VistaPedido {
 	public Pedidos _pedidos;
 	public VerticalLayout layout;
-	public Select<String> valoracion = new Select<>();
+	base_de_datos.Compra compra;
 	
-	public Pedido() {
+	public Pedido(base_de_datos.Compra compra) {
 		this.layout = this.getVaadinVerticalLayout().as(VerticalLayout.class);
 		
-		//if(estado == x )
-		valoracion.setItems("0", "1", "2", "3", "4", "5");
-		//Cuando se valore guardarlo en la base de datos
-		valoracion.addValueChangeListener(event ->{
+		this.compra = compra;
+		
+		if(this.compra instanceof base_de_datos.Recibido) {
+			this.getEstado_pedido().setText("Recibido");
+			this.getBoton_cancelar_pedido().setVisible(false);
 			
-		});
-		this.layout.add(valoracion);
+			
+			
+		}
+		
+		if(this.compra instanceof base_de_datos.Pendiente) {
+			this.getEstado_pedido().setText("Pendiente");
+		}
+		
+		if(this.compra instanceof base_de_datos.Enviado) {
+			this.getEstado_pedido().setText("Enviado");
+			this.getBoton_cancelar_pedido().setVisible(false);
+		}
 	}
 	
 
