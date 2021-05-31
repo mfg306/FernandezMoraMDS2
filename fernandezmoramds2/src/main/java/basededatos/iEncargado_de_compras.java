5cfbc35c4ac77150062a6b8626dba9c82bc3c32b
Marta Fernández <52571378+mfg306@users.noreply.github.com>
2021-05-31 10:30:30 +0200

    Update iEncargado_de_compras.java

--- fernandezmoramds2/src/main/java/basededatos/iEncargado_de_compras.java
+++ fernandezmoramds2/src/main/java/basededatos/iEncargado_de_compras.java
@@ -11,15 +11,15 @@ public interface iEncargado_de_compras {
 
 	public Pendiente[] cargarPedidosPendientes(int aIdEncargado);
 
-	public void asignarPedidoTransportista(Pendiente aPedidoPendiente, Transportista aTransportista, Encargado_de_compras aEncargado) throws PersistentException;
+	public void asignarPedidoTransportista(Pendiente aPedidoPendiente, Encargado_de_compras aEncargado) throws PersistentException;
 
 	public Pendiente[] cargarPedidosE(int aIdEncargado) throws PersistentException;
 
-	public Transportista[] cargarTransportistas() throws PersistentException;
-	
 	public boolean eliminarPendiente(Pendiente aPedidoPendiente) throws PersistentException;
 	
 	public boolean eliminarProductosEnCompra(Pendiente aPendiente) throws PersistentException;
 	
 	public Producto_en_compra[] cargarProductosEnCompra(int aIdEnviado) throws PersistentException;
+	
+	public Transportista buscarTransportistaCola(int aIdCola) throws PersistentException;
 }
